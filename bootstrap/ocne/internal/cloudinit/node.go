4f6f0a27a6f84bfa7874847dc67fd9eed94491b7
Abhishek Mitra <abhishek.a.mitra@oracle.com>
2023-05-26 00:24:27 -0700

    VZ-9879: Make OCNE overrides independant of Pre and Post OCNE commands for node

--- bootstrap/ocne/internal/cloudinit/node.go
+++ bootstrap/ocne/internal/cloudinit/node.go
@@ -32,6 +32,7 @@ const (
     permissions: '0640'
     content: "This placeholder file is used to create the /run/cluster-api sub directory in a way that is compatible with both Linux and Windows (mkdir -p /run/cluster-api does not work with Windows)"
 runcmd:
+{{- template "commands" .OCNEOverrides }}
 {{- template "commands" .PreOCNECommands }}
   - {{ .OCNECommand }} && {{ .SentinelFileCommand }}
 {{- template "commands" .PostOCNECommands }}
