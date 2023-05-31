68be86f9c86bf90b8f8e1d85b742c9edc8654c10
Abhishek Mitra <abhishek.a.mitra@oracle.com>
2023-05-31 08:26:45 -0700

    VZ-9912: webhook allow WIP

--- controlplane/ocne/api/v1alpha1/ocne_control_plane_webhook.go
+++ controlplane/ocne/api/v1alpha1/ocne_control_plane_webhook.go
@@ -115,26 +115,27 @@ func (in *OCNEControlPlane) ValidateCreate() error {
 }
 
 const (
-	spec                 = "spec"
-	controlPlaneConfig   = "controlPlaneConfig"
-	clusterConfiguration = "clusterConfiguration"
-	initConfiguration    = "initConfiguration"
-	joinConfiguration    = "joinConfiguration"
-	nodeRegistration     = "nodeRegistration"
-	skipPhases           = "skipPhases"
-	patches              = "patches"
-	directory            = "directory"
-	preOCNECommands      = "preOCNECommands"
-	postOCNECommands     = "postOCNECommands"
-	files                = "files"
-	users                = "users"
-	apiServer            = "apiServer"
-	controllerManager    = "controllerManager"
-	scheduler            = "scheduler"
-	ntp                  = "ntp"
-	ignition             = "ignition"
-	diskSetup            = "diskSetup"
-	ocneModuleOperator   = "ocneModuleOperator"
+	spec                     = "spec"
+	controlPlaneConfig       = "controlPlaneConfig"
+	clusterConfiguration     = "clusterConfiguration"
+	initConfiguration        = "initConfiguration"
+	joinConfiguration        = "joinConfiguration"
+	nodeRegistration         = "nodeRegistration"
+	skipPhases               = "skipPhases"
+	patches                  = "patches"
+	directory                = "directory"
+	preOCNECommands          = "preOCNECommands"
+	postOCNECommands         = "postOCNECommands"
+	files                    = "files"
+	users                    = "users"
+	apiServer                = "apiServer"
+	controllerManager        = "controllerManager"
+	scheduler                = "scheduler"
+	ntp                      = "ntp"
+	ignition                 = "ignition"
+	diskSetup                = "diskSetup"
+	ocneModuleOperator       = "ocneModuleOperator"
+	verrazzanoModuleOperator = "verrazzanoModuleOperator"
 )
 
 const minimumCertificatesExpiryDays = 7
@@ -182,6 +183,8 @@ func (in *OCNEControlPlane) ValidateUpdate(old runtime.Object) error {
 		{spec, "rolloutStrategy", "*"},
 		{spec, ocneModuleOperator},
 		{spec, ocneModuleOperator, "*"},
+		{spec, verrazzanoModuleOperator},
+		{spec, verrazzanoModuleOperator, "*"},
 	}
 
 	allErrs := validateKubeadmControlPlaneSpec(in.Spec, in.Namespace, field.NewPath("spec"))
